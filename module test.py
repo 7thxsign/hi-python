41bef189945c043b8fc49e6ec4466dc610171497
Mithil <mithilabhiram404@gmail.com>
2025-10-25 21:23:38 +0530

    modules

---
+++ module test.py
@@ -0,0 +1,4 @@
+import module_example
+from module_example import square, circumference
+
+print(circumference(2))
---
+++ module_example.py
@@ -0,0 +1,15 @@
+pi = 3.14159
+
+def square(x):
+    x ** 2
+    return x
+
+def cube(x):
+    x ** 3
+    return x
+
+def multiply(x, y):
+    return x * y
+
+def circumference(x):
+    return 2 * pi * x
