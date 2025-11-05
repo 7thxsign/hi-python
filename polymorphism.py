c46bb496c9faa31bb4bde939994405fcab68e28a
Mithil Abhiram <mithilabhiram404@gmail.com>
2025-11-05 18:57:19 +0530

    polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * (self.radius ** 2)
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
+
+class Pizza(Circle):
+    def __init__(self, topping, radius):
+        self.topping = topping
+        super().__init__(radius)
+
+
+shapes = [Circle(2), Square(3), Triangle(4, 5), Pizza("Pepperoni", 6)]
+
+for shape in shapes:
+    print(f'The area of {shape} is {shape.area()}')
